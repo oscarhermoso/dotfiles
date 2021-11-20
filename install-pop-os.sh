d2ed89773a8b309c67923be1ca1400bd414e65a8
Oscar Hermoso <oscarhermoso852@gmail.com>
2021-11-20 17:22:34 +0800

    Docker config, some other apt installs

--- install-pop-os.sh
+++ install-pop-os.sh
@@ -6,7 +6,7 @@ mkdir -p .local/bin
 
 
 # Apt Installs
-sudo apt install vim gnome-tweaks code firefox discord 
+sudo apt install vim gnome-tweaks code firefox discord fonts-firacode obs-studio
 
 
 # https://www.microsoftedgeinsider.com/en-us/download?platform=linux-deb
@@ -16,15 +16,11 @@ sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge sta
 sudo rm microsoft.gpg
 sudo apt update
 sudo apt install microsoft-edge-beta
-
-
-# https://support.gitkraken.com/how-to-install
-wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
-sudo dpkg -i gitkraken-amd64.deb
+xdg-settings set default-web-browser microsoft-edge-beta.desktop
 
 
 # https://www.spotify.com/au/download/linux/
-curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add - 
+curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add -
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt-get update && sudo apt-get install spotify-client
 
@@ -32,13 +28,16 @@ sudo apt-get update && sudo apt-get install spotify-client
 # https://bitwarden.com/download/
 curl -s https://api.github.com/repos/bitwarden/desktop/releases/latest \
 | grep "browser_download_url.*AppImage" \
-| cut -d '"' -f 4 \	
+| cut -d '"' -f 4 \
 | wget -i -
 chmod a+x Bitwarden*AppImage
 mv Bitwarden*AppImage $HOME/.local/bin/
 
 
 # https://docs.docker.com/engine/install/ubuntu/
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
 sudo apt install ca-certificates curl gnupg lsb-release
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo \
@@ -46,3 +45,5 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
 sudo apt install docker-ce docker-ce-cli containerd.io
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
