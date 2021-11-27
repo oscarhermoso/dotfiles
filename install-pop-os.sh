44df7c546abe5f3889c146babaa4a108067f487e
Oscar Hermoso <oscarhermoso852@gmail.com>
2021-11-27 11:49:48 +0800

    Curl helm installation ("living on the edge")

--- install-pop-os.sh
+++ install-pop-os.sh
@@ -47,3 +47,7 @@ sudo apt update
 sudo apt install docker-ce docker-ce-cli containerd.io
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+
+# https://helm.sh/docs/intro/install/
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
